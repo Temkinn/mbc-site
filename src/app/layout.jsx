53e60984d4b1e95b87bc137f6553c5e563ee9426
unknown <artemaskel@gmail.com>
2024-12-27 20:45:05 +0300

    kwa

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css"
+import Head from "next/head";
 
 export const metadata = {
   title: "MBC Кофейня",
@@ -9,6 +10,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+      </Head>
       <body>
         {children}
       </body>
