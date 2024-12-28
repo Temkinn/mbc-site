55abde4ce36fa642bcbfd3685fff14c85bb2843f
unknown <artemaskel@gmail.com>
2024-12-28 18:53:43 +0300

    last

--- src/components/menu/items/photos.js
+++ src/components/menu/items/photos.js
@@ -1,30 +1,30 @@
-import americano from "./photos/americano1.jpg"
-import classic from './photos/classic.jpg'
-import aeropress from "./photos/aeropress.jpg"
-import cherry from "./photos/cherry.jpg"
-import espresso from './photos/espresso.jpg'
+import americano from "./photos/black/americano1.jpg"
+import classic from './photos/black/classic.jpg'
+import aeropress from "./photos/black/aeropress.jpg"
+import cherry from "./photos/black/cherry.jpg"
+import espresso from './photos/black/espresso.jpg'
 
-import flat_white from "./photos/flat_white.jpg"
-import latte from "./photos/latte.jpg"
-import raf_rasp from "./photos/raf_rasp.jpg"
-import raf_vanila from "./photos/raf_vanila.jpg"
+import flat_white from "./photos/milk/flat_white.jpg"
+import latte from "./photos/milk/latte.jpg"
+import raf_rasp from "./photos/milk/raf_rasp.jpg"
+import raf_vanila from "./photos/milk/raf_vanila.jpg"
+import lolipop from "./photos/milk/lolipop.jpg"
 
 
 const photos = {
   black: {
-	americano: americano,
-	classic: classic,
-	aeropress: aeropress,
-	cherry: cherry,
-	espresso: espresso,
-  },
-  milk: {
+    americano: americano,
+    classic: classic,
+    aeropress: aeropress,
+    cherry: cherry,
+    espresso: espresso,
   },
   milk : {
-	flat_white: flat_white,
-	latte: latte,
+	  flat_white: flat_white,
+	  latte: latte,
     raf_rasp: raf_rasp,
     raf_vanila: raf_vanila,
+    lolipop: lolipop,
   },
   white: {
   },
