aa2c644186fc146a8b002830e24e58188ba0ae77
unknown <artemaskel@gmail.com>
2024-12-28 16:04:34 +0300

    last

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -11,6 +11,19 @@ import photo5 from './photos/5.jpg'
 import photo6 from './photos/6.jpg'
 
 export default function Slider(){
+	let k = 0
+
+	if(window.innerWidth <= 320){
+		k = 103.2
+	} else if(window.innerWidth <= 375){
+		k = 88
+	} else if(window.innerWidth < 597){
+		k = 77
+	}  else if(window.innerWidth < 896){
+		k = 42.5
+	}  if(window.innerWidth >= 896){
+		k = 30.5
+	}
 
 	const slides = [
 		{
@@ -47,6 +60,7 @@ export default function Slider(){
 
 	const next = () => {
 		setCurr(curr => curr === slides.length - 1 ? 0 : curr + 1)
+		console.log(k);
 	}
 
 	return(
@@ -55,7 +69,7 @@ export default function Slider(){
 			<div className={styles.left} onClick={prev} id={styles.button} />
 			<div className={styles.sliderContainer}>
 				<div className={styles.slider} style={{
-					transform: `translateX(-${curr * 24.5}%)`,
+					transform: `translateX(-${curr * k}%)`,
 					transition: 'transform .5s ease-in-out',
 				}}>	
 					{slides.map(
