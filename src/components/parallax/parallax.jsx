e12f2e2c59e96282071bcbed6d7e6d00b03c01b0
unknown <artemaskel@gmail.com>
2025-01-04 17:12:14 +0300

    last

--- src/components/parallax/parallax.jsx
+++ src/components/parallax/parallax.jsx
@@ -1,7 +1,12 @@
 import styles from "./styles.module.css"
+import Image from "next/image"
 
 export default function Parallax(){
-	return(
+	return(<>
 		<div className={styles.parallax}></div>
+		<a href="#" className={styles.up}>
+			<Image src={require("./arrows5.png")} alt="up" fill />
+		</a>
+		</>
 	)
 }
