32b3065e9bbe150a9a3d928a7da5fa1e31abde57
Artyom Yaskel <artemaske@gmail.com>
2025-02-12 21:56:52 +0300

    last

--- src/components/menu/position.jsx
+++ src/components/menu/position.jsx
@@ -6,13 +6,13 @@ export default function Position({name, photo, price, kpfc, description, amount}
 		<div key={name} className={styles.item}>
 								<div className={styles.photo}>
 									<div className={styles.front}>
-										<Image src={photo} alt={name} loading='eager' fill/>
+										<Image src={photo} alt={name} fill/>
 									</div>
 									<div className={styles.desc}>
 										<div className={styles.descInner}>
 											<div className={styles.description}>{description}</div>
 											<div className={styles.kpfc}><p>{kpfc}</p></div>
-										</div>
+										</div> 
 									</div>
 								</div>
 		  <div className={styles.info}>
