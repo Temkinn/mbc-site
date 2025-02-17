b731b70cef93d17f0312ca5b83c4a5da852fa1c2
Artyom Yaskel <artemaske@gmail.com>
2025-02-18 00:06:28 +0300

    last

--- src/components/menu/position.jsx
+++ src/components/menu/position.jsx
@@ -1,30 +1,28 @@
-import styles from './position.module.css'
+import styles from './position.module.scss'
 import Image from 'next/image'
 
 export default function Position({name, photo, price, kpfc, description, amount}){
 	return (
     <div key={name} className={styles.item}>
-      <div className={styles.photo}>
-        <div className={styles.photocontainer} unselectable="true">
-          <div className={styles.front} unselectable="true">
-            <Image
-              src={photo}
-              alt={name}
-              loading="lazy"
-              placeholder="blur"
-              fill
-            />
-          </div>
-          <div className={styles.desc} unselectable="true">
-            <div className={styles.descInner} unselectable="true">
-              <div className={styles.description}>{description}</div>
-              <div className={styles.kpfc}>
-                <p>{kpfc}</p>
+          <div className={styles.photo}>
+              <div className={styles.front} unselectable="true">
+                <Image
+                  src={photo}
+                  alt={name}
+                  loading="lazy"
+                  placeholder="blur"
+                  fill
+                />
               </div>
-            </div>
+              <div className={styles.desc} unselectable="true">
+                <div className={styles.descInner} unselectable="true">
+                  <div className={styles.description}>{description}</div>
+                  <div className={styles.kpfc}>
+                    <p>{kpfc}</p>
+                  </div>
+                </div>
+              </div>
           </div>
-        </div>
-      </div>
       <div className={styles.info}>
         <div className={styles.name}>{name}</div>
         <div className={styles.g}>{amount}</div>
