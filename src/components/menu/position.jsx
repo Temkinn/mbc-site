f6416eed2e15a46b5e14ce00bfb0629afbac45a6
Artyom Yaskel <artemaske@gmail.com>
2025-02-17 23:19:49 +0300

    last

--- src/components/menu/position.jsx
+++ src/components/menu/position.jsx
@@ -3,25 +3,33 @@ import Image from 'next/image'
 
 export default function Position({name, photo, price, kpfc, description, amount}){
 	return (
-		<div key={name} className={styles.item}>
-			<div className={styles.photo}>
-				<div className={styles.photocontainer}>
-					<div className={styles.front}>
-						<Image src={photo} alt={name} loading="lazy" placeholder="blur" fill/>
-					</div>
-					<div className={styles.desc}>
-						<div className={styles.descInner}>
-							<div className={styles.description}>{description}</div>
-							<div className={styles.kpfc}><p>{kpfc}</p></div>
-						</div> 
-					</div>
-				</div>
-			</div>
-		  <div className={styles.info}>
-			<div className={styles.name}>{name}</div>
-			<div className={styles.g}>{amount}</div>
-			<div className={styles.price}>{price} руб.</div>
-		  </div>
-		</div>
-	  );
+    <div key={name} className={styles.item}>
+      <div className={styles.photo}>
+        <div className={styles.photocontainer} unselectable="true">
+          <div className={styles.front} unselectable="true">
+            <Image
+              src={photo}
+              alt={name}
+              loading="lazy"
+              placeholder="blur"
+              fill
+            />
+          </div>
+          <div className={styles.desc} unselectable="true">
+            <div className={styles.descInner} unselectable="true">
+              <div className={styles.description}>{description}</div>
+              <div className={styles.kpfc}>
+                <p>{kpfc}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.info}>
+        <div className={styles.name}>{name}</div>
+        <div className={styles.g}>{amount}</div>
+        <div className={styles.price}>{price} руб.</div>
+      </div>
+    </div>
+  );
 }
