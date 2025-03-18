d88a84fa9ef40931023ff99517a01dc0d37b0563
Temkinn <artemaskel@gmail.com>
2025-03-18 19:07:35 +0300

    menu updated

--- src/components/barista/barista.jsx
+++ src/components/barista/barista.jsx
@@ -15,7 +15,6 @@ export default function Barista(){
 			<div className={styles.people}>
 				<Person name="Илья Гринкевич" photo={Ilya} link="https://www.instagram.com/ilyagrink/?igsh=MXBtZTkzZWE1NmZkNw%3D%3D#"/>
 				<Person name="Антон Быстрицкий" photo={Anton} link="https://www.instagram.com/tohabystricki/?igsh=aXZueGluMW5oem85"/>
-				<Person name="Даниил Котляров" photo={Daniil} link="https://www.instagram.com/daniilkatletka?igsh=Y21odWhqMGVlM3gz"/>
 				<Person name="Анастасия Русакевич" photo={Anastasia} link="https://www.instagram.com/nastya_rusakevich23?igsh=MWQxcml0bDNoaHM0dg=="/>
 				<Person name="Алена Зиневич" photo={Alena} link="https://www.instagram.com/aliona_zinevich/profilecard/?igsh=MTZ0bHRneGMzbzE3MQ%3D%3D"/>
 			</div>
