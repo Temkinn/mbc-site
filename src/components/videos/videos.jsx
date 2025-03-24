b37fe6203cc759946f2cf2d4f14ae64cdf3acdf0
Temkinn <artemaskel@gmail.com>
2025-03-25 01:12:25 +0300

    more optimization

--- src/components/videos/videos.jsx
+++ src/components/videos/videos.jsx
@@ -1,13 +1,31 @@
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
 
-export default function Videos(){
-	return(
-        <div className={styles.container}>
+export default function Videos() {
+  return (
+    <div className={styles.container}>
+      <iframe
+        className={styles.video1}
+        width="560"
+        height="400"
+        src="https://www.youtube.com/embed/Ap7ru9ZiDC0?si=l3xAV2rC6x0iWdq1"
+        loading="lazy"
+        title="MBC Culture"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+        ></iframe>
 
-			<iframe className={styles.video1} width="560" height="400" src="https://www.youtube.com/embed/Ap7ru9ZiDC0?si=l3xAV2rC6x0iWdq1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-
-			<iframe className={styles.video2} width="560" height="400" src="https://www.youtube.com/embed/_W2WqHKtgTw?si=jPMjf8qbQ_8v7rYL" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-
-        </div>
-    )
+      <iframe
+        className={styles.video2}
+        width="560"
+        height="400"
+        src="https://www.youtube.com/embed/_W2WqHKtgTw?si=jPMjf8qbQ_8v7rYL"
+        loading="lazy"
+        title="MBC Culture"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 }
--- src/components/welcome/welcome.jsx
+++ src/components/welcome/welcome.jsx
@@ -1,21 +1,10 @@
 "use client";
-import Image from "next/image";
 import styles from "./styles.module.css";
-import { useState, useEffect } from "react";
 
 export default function Welcome() {
 
-	const [width, setWidth] = useState(0);
-
-	useEffect(() => {
-		setWidth(window.innerWidth)
-		return () => {
-
-		}
-	}, [])
   const goTo = (name) => {
     const unit = document.querySelector(`#${name}`);
-    console.log(width);
 
     if (unit) {
       unit.scrollIntoView({ behavior: "smooth" });
@@ -24,22 +13,6 @@ export default function Welcome() {
 
   return (
     <div className={styles.container}>
-      <Image
-        src={
-          width > 500
-            ? require("@/../public/welcome_background.jpg")
-            : require("@/../public/welcome2_background.jpg")
-        }
-        alt=""
-        placeholder="blur"
-        quality={100}
-        fill
-        sizes="100vw"
-        style={{
-          objectFit: "cover",
-          zIndex: -5,
-        }}
-      />
       <div className={styles.titles}>
         <h1 className={styles.title}>MBC.CULTURE</h1>
         <h2 className={styles.subtitle}>Кофейня на каждый день</h2>
