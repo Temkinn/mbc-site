53419254aa47a69db9e66b9e78036e2497abb604
Temkinn <artemaskel@gmail.com>
2025-04-02 19:14:25 +0300

    menu updated!

--- src/components/menu/items/photos.js
+++ src/components/menu/items/photos.js
@@ -1,35 +1,38 @@
 // black
-import americano from "./photos/black/americano.jpg"
-import el_americano from "./photos/black/el_americano.jpg"
-import classic from './photos/black/classic.jpg'
-import aeropress from "./photos/black/aeropress.jpg"
-import cherry from "./photos/black/cherry.jpg"
-import espresso from './photos/black/espresso.jpg'
+import americano from "./photos/black/americano.jpg";
+import el_americano from "./photos/black/el_americano.jpg";
+import classic from "./photos/black/classic.jpg";
+import aeropress from "./photos/black/aeropress.jpg";
+import cherry from "./photos/black/cherry.jpg";
+import espresso from "./photos/black/espresso.jpg";
 
 // milk
-import flat_white from "./photos/milk/flat_white.jpg"
-import latte from "./photos/milk/latte.jpg"
-import raf_rasp from "./photos/milk/raf_rasp.jpg"
-import raf_vanila from "./photos/milk/raf_vanila.jpg"
-import lolipop from "./photos/milk/lolipop.jpg"
-import halva_latte from "./photos/milk/halva_latte.jpg"
-import caramel_latte from './photos/milk/caramel_latte.jpg'
-import kap from './photos/milk/kap.jpg'
-import tropic_flat from './photos/milk/tropic_flat.jpg'
+import flat_white from "./photos/milk/flat_white.jpg";
+import latte from "./photos/milk/latte.jpg";
+import raf_rasp from "./photos/milk/raf_rasp.jpg";
+import raf_vanila from "./photos/milk/raf_vanila.jpg";
+import lolipop from "./photos/milk/lolipop.jpg";
+import halva_latte from "./photos/milk/halva_latte.jpg";
+import caramel_latte from "./photos/milk/caramel_latte.jpg";
+import kap from "./photos/milk/kap.jpg";
+import tropic_flat from "./photos/milk/tropic_flat.jpg";
 
 // other
-import orangej from './photos/other/orangej.jpg'
-import raspb_cocao from "./photos/other/raspb_cocao.jpg"
-import cocao_v from "./photos/other/cocao_v.jpg"
-import kid_cocao from "./photos/other/kid_cocao.jpg"
-import hot_ch from "./photos/other/hot_ch.jpg"
-import matcha_orange from "./photos/other/matcha_orange.jpg"
-import matcha_raf from "./photos/other/matcha_raf.jpg"
-import matcha_latte from "./photos/other/matcha_latte.jpg"
-import glintvein_ba from "./photos/other/glintvein_ba.jpg"
+import orangej from "./photos/other/orangej.jpg";
+import raspb_cocao from "./photos/other/raspb_cocao.jpg";
+import cocao_v from "./photos/other/cocao_v.jpg";
+import kid_cocao from "./photos/other/kid_cocao.jpg";
+import hot_ch from "./photos/other/hot_ch.jpg";
+import matcha_orange from "./photos/other/matcha_orange.jpg";
+import matcha_raf from "./photos/other/matcha_raf.jpg";
+import matcha_latte from "./photos/other/matcha_latte.jpg";
+import glintvein_ba from "./photos/other/glintvein_ba.jpg";
+import cold_bru from "./photos/other/cold_bru.jpg"
+import matcha_bumble from "./photos/other/matcha_bumble.jpg"
+import orange_bumble from "./photos/other/orange_bumble.jpg"
 
 //special
-import asian_tea from './photos/special/asian_tea.jpg'
+import asian_tea from "./photos/special/asian_tea.jpg";
 import dubai_americano from "./photos/special/dubai_americano.jpg";
 import matcha_sakura from "./photos/special/matcha_sakura.jpg";
 import mint_berry from "./photos/special/mint_berry.jpg";
@@ -37,31 +40,28 @@ import spanish_latte from "./photos/special/spanish_latte.jpg";
 import sun_aloe from "./photos/special/sun_aloe.jpg";
 
 // tea
-import raspb from "./photos/tea/raspb.jpg"
-import tea_mint from "./photos/tea/tea_mint.jpg"
-import obl_tea from "./photos/tea/obl_tea.jpg"
-import obl_baunty from "./photos/tea/obl_baunty.jpg"
-import imb_el from "./photos/tea/imb_el.jpg"
-import aloe from './photos/tea/aloe1.jpg'
-import leaves from './photos/tea/leaves.jpg'
-
+import raspb from "./photos/tea/raspb.jpg";
+import tea_mint from "./photos/tea/tea_mint.jpg";
+import obl_tea from "./photos/tea/obl_tea.jpg";
+import obl_baunty from "./photos/tea/obl_baunty.jpg";
+import imb_el from "./photos/tea/imb_el.jpg";
+import aloe from "./photos/tea/aloe1.jpg";
+import leaves from "./photos/tea/leaves.jpg";
 
 //breakfask
-import croissant_v from "./photos/breakfast/croissant_v.jpg"
-import croissant_mindal from "./photos/breakfast/croissant_mindal.jpg"
-import croissant_chick from "./photos/breakfast/croissant_chick.jpg"
-import croissant_fund from "./photos/breakfast/croissant_fund.jpg"
-import croissant_redfish from "./photos/breakfast/croissant_redfish.jpg"
-import vanil_sir from './photos/breakfast/vanil_sir.jpg'
-import cheese_sir from './photos/breakfast/cheese_sir.jpg'
-
+import croissant_v from "./photos/breakfast/croissant_v.jpg";
+import croissant_mindal from "./photos/breakfast/croissant_mindal.jpg";
+import croissant_chick from "./photos/breakfast/croissant_chick.jpg";
+import croissant_fund from "./photos/breakfast/croissant_fund.jpg";
+import croissant_redfish from "./photos/breakfast/croissant_redfish.jpg";
+import vanil_sir from "./photos/breakfast/vanil_sir.jpg";
+import cheese_sir from "./photos/breakfast/cheese_sir.jpg";
 
 //sweet
-import banana_keks from "./photos/sweet/banana_keks.jpg"
-import limon_keks from "./photos/sweet/limon_keks.jpg"
-import salt_iris from "./photos/sweet/salt_iris.jpg"
-import choco_iris from "./photos/sweet/choco_iris.jpg"
-
+import banana_keks from "./photos/sweet/banana_keks.jpg";
+import limon_keks from "./photos/sweet/limon_keks.jpg";
+import salt_iris from "./photos/sweet/salt_iris.jpg";
+import choco_iris from "./photos/sweet/choco_iris.jpg";
 
 const photos = {
   black: {
@@ -72,9 +72,9 @@ const photos = {
     cherry: cherry,
     espresso: espresso,
   },
-  milk : {
-	  flat_white: flat_white,
-	  latte: latte,
+  milk: {
+    flat_white: flat_white,
+    latte: latte,
     raf_rasp: raf_rasp,
     raf_vanila: raf_vanila,
     lolipop: lolipop,
@@ -93,7 +93,10 @@ const photos = {
     matcha_raf: matcha_raf,
     matcha_latte: matcha_latte,
     glintvein_ba: glintvein_ba,
-  }, 
+    cold_bru,
+    matcha_bumble,
+    orange_bumble,
+  },
   special: {
     asian_tea: asian_tea,
     dubai_americano: dubai_americano,
@@ -125,7 +128,7 @@ const photos = {
     limon_keks: limon_keks,
     salt_iris: salt_iris,
     choco_iris: choco_iris,
-  }
-}
+  },
+};
 
-export default photos
+export default photos;
